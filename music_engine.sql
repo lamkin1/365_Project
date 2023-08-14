8ac495c793bb28dab1863474f99879e26f1b68a8
Naomi <97060752+nam0mi@users.noreply.github.com>
2023-08-14 11:22:43 -0700

    create tables

--- music_engine.sql
+++ music_engine.sql
@@ -1,1 +1,51 @@
-CREATE TABLE
+CREATE TABLE Artists (
+	id INT AUTO_INCREMENT,
+    artist_name varchar(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Genres (
+	id INT AUTO_INCREMENT,
+    genre_name varchar(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Albums (
+	id INT AUTO_INCREMENT,
+    album_name varchar(50) NOT NULL,
+    artist_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(artist_id) REFERENCES Artists(id)
+);
+
+CREATE TABLE Songs (
+	id INT AUTO_INCREMENT,
+    song_title varchar(50) NOT NULL,
+    release_date DATE NOT NULL,
+    artist_id INT NOT NULL,
+    album_id INT,
+    duration INT NOT NULL,
+    genre_id INT NOT NULL,
+    url varchar(100),
+    FOREIGN KEY (artist_id) REFERENCES Artists(id),
+    FOREIGN KEY (album_id) REFERENCES Albums(id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Playlists (
+	id INT AUTO_INCREMENT,
+    playlist_name varchar(50) NOT NULL,
+    date_created DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE PlaylistSongs (
+	id INT AUTO_INCREMENT,
+    playlist_id INT NOT NULL,
+    song_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(playlist_id) REFERENCES Playlists(id),
+    FOREIGN KEY(song_id) REFERENCES Songs(id)
+);
+
