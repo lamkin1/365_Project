0ffc67a6a05bb04ab757e1c627dc274b2cc6ba71
lamkin1 <jamesclamkin@gmail.com>
2023-08-21 11:10:14 -0700

    Added Eras Table

--- SQLInitializer.java
+++ SQLInitializer.java
@@ -3,22 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.csc365p1;
+
 import java.sql.*;
 import java.util.ArrayList;
 import com.mycompany.csc365p1.DatabaseConnection;
 
-
 /**
  *
  * @author seansponsler
- * small class to initialize SQL tables on independent system
+ *         small class to initialize SQL tables on independent system
  */
 public class SQLInitializer {
     private DatabaseConnection databaseConnection;
     private Connection connection;
     private int i;
     private static ArrayList<String> tables;
-    
+
     public SQLInitializer() {
         i = 0;
         tables = new ArrayList();
@@ -26,7 +26,7 @@ public class SQLInitializer {
         this.databaseConnection = new DatabaseConnection();
         this.connection = this.databaseConnection.getConnection();
     }
-    
+
     public void initialize() {
         try {
             System.out.println("TABLES SIZE: " + tables.size());
@@ -37,20 +37,20 @@ public class SQLInitializer {
                 i++;
             }
         } catch (SQLException e) {
-            //ugly recursion since we expect some SQLExceptions in this initialization
+            // ugly recursion since we expect some SQLExceptions in this initialization
             e.printStackTrace();
             i++;
             if (i > 100) {
-                //infinite catch
+                // infinite catch
                 System.err.println("Something went horribly wrong. i = " + i);
                 return;
             }
             initialize();
         }
     }
-    
+
     public void initializeTables() {
-        //artist_name
+        // artist_name
         String createArtistsTable = "CREATE TABLE Artists ("
                 + "id INT AUTO_INCREMENT,"
                 + "artist_name VARCHAR(50) NOT NULL,"
@@ -63,6 +63,12 @@ public class SQLInitializer {
                 + "PRIMARY KEY (id)"
                 + ")";
         tables.add(createGenresTable);
+        String createErasTable = "CREATE TABLE Genres ("
+                + "id INT AUTO_INCREMENT,"
+                + "era VARCHAR(8) NOT NULL,"
+                + "PRIMARY KEY (id)"
+                + ")";
+        tables.add(createErasTable);
         String createAlbumsTable = "CREATE TABLE Albums ("
                 + "id INT AUTO_INCREMENT,"
                 + "album_name VARCHAR(50) NOT NULL,"
@@ -71,7 +77,7 @@ public class SQLInitializer {
                 + "FOREIGN KEY(artist_id) REFERENCES Artists(id)"
                 + ")";
         tables.add(createAlbumsTable);
-        //id, song_title, artist_id, album_id, duration(int), genre_id, url(varchar)
+        // id, song_title, artist_id, album_id, duration(int), genre_id, url(varchar)
         String createSongsTable = "CREATE TABLE Songs ("
                 + "id INT AUTO_INCREMENT,"
                 + "song_title VARCHAR(50) NOT NULL,"
@@ -101,6 +107,6 @@ public class SQLInitializer {
                 + "FOREIGN KEY (playlist_id) REFERENCES Playlists(id),"
                 + "FOREIGN KEY(song_id) REFERENCES Songs(id)"
                 + ")";
-        tables.add(createPlaylistSongsTable);   
+        tables.add(createPlaylistSongsTable);
     }
 }
