494dd4d8ca59ab2f799dd0d1a6aaa61a3b6575c2
Sean Sponsler <ssponsler1998@gmail.com>
2023-08-20 18:01:17 -0700

    Create SQLInitializer.java
    small, non-critical file to initialize on new systems

---
+++ SQLInitializer.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc365p1;
+import java.sql.*;
+import java.util.ArrayList;
+import com.mycompany.csc365p1.DatabaseConnection;
+
+
+/**
+ *
+ * @author seansponsler
+ * small class to initialize SQL tables on independent system
+ */
+public class SQLInitializer {
+    private DatabaseConnection databaseConnection;
+    private Connection connection;
+    private int i;
+    private static ArrayList<String> tables;
+    
+    public SQLInitializer() {
+        i = 0;
+        tables = new ArrayList();
+        initializeTables();
+        this.databaseConnection = new DatabaseConnection();
+        this.connection = this.databaseConnection.getConnection();
+    }
+    
+    public void initialize() {
+        try {
+            System.out.println("TABLES SIZE: " + tables.size());
+            while (i < tables.size()) {
+                PreparedStatement createStmt = connection.prepareStatement(tables.get(i));
+                createStmt.executeUpdate();
+                createStmt.close();
+                i++;
+            }
+        } catch (SQLException e) {
+            //ugly recursion since we expect some SQLExceptions in this initialization
+            e.printStackTrace();
+            i++;
+            if (i > 100) {
+                //infinite catch
+                System.err.println("Something went horribly wrong. i = " + i);
+                return;
+            }
+            initialize();
+        }
+    }
+    
+    public void initializeTables() {
+        //artist_name
+        String createArtistsTable = "CREATE TABLE Artists ("
+                + "id INT AUTO_INCREMENT,"
+                + "artist_name VARCHAR(50) NOT NULL,"
+                + "PRIMARY KEY (id)"
+                + ")";
+        tables.add(createArtistsTable);
+        String createGenresTable = "CREATE TABLE Genres ("
+                + "id INT AUTO_INCREMENT,"
+                + "genre_name VARCHAR(50) NOT NULL,"
+                + "PRIMARY KEY (id)"
+                + ")";
+        tables.add(createGenresTable);
+        String createAlbumsTable = "CREATE TABLE Albums ("
+                + "id INT AUTO_INCREMENT,"
+                + "album_name VARCHAR(50) NOT NULL,"
+                + "artist_id INT NOT NULL,"
+                + "PRIMARY KEY (id),"
+                + "FOREIGN KEY(artist_id) REFERENCES Artists(id)"
+                + ")";
+        tables.add(createAlbumsTable);
+        //id, song_title, artist_id, album_id, duration(int), genre_id, url(varchar)
+        String createSongsTable = "CREATE TABLE Songs ("
+                + "id INT AUTO_INCREMENT,"
+                + "song_title VARCHAR(50) NOT NULL,"
+                + "artist_id INT NOT NULL,"
+                + "album_id INT,"
+                + "duration INT NOT NULL,"
+                + "genre_id INT NOT NULL,"
+                + "url VARCHAR(100),"
+                + "PRIMARY KEY (id),"
+                + "FOREIGN KEY (artist_id) REFERENCES Artists(id),"
+                + "FOREIGN KEY (album_id) REFERENCES Albums(id),"
+                + "FOREIGN KEY (genre_id) REFERENCES Genres(id)"
+                + ")";
+        tables.add(createSongsTable);
+        String createPlaylistsTable = "CREATE TABLE Playlists ("
+                + "id INT AUTO_INCREMENT,"
+                + "playlist_name VARCHAR(50) NOT NULL,"
+                + "date_created DATE NOT NULL,"
+                + "PRIMARY KEY (id)"
+                + ")";
+        tables.add(createPlaylistsTable);
+        String createPlaylistSongsTable = "CREATE TABLE PlaylistSongs ("
+                + "id INT AUTO_INCREMENT,"
+                + "playlist_id INT NOT NULL,"
+                + "song_id INT NOT NULL,"
+                + "PRIMARY KEY (id),"
+                + "FOREIGN KEY (playlist_id) REFERENCES Playlists(id),"
+                + "FOREIGN KEY(song_id) REFERENCES Songs(id)"
+                + ")";
+        tables.add(createPlaylistSongsTable);   
+    }
+}
