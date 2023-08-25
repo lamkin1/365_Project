b510f24de8e1efa72d041b4b354d1ca935036838
Sean Sponsler <ssponsler1998@gmail.com>
2023-08-25 11:11:38 -0700

    Create Era.java

---
+++ Era.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc365p1;
+
+/**
+ *
+ * @author seansponsler
+ */
+public class Era {
+    private String era;
+    public Era(String era_name) { this.era = era_name; }
+    public String getEra() { return this.era; }
+    public void setEraName(String era_name) { this.era = era_name; }
+}
